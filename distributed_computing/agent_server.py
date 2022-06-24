10d683432737baefd76854e6c94983db5c68a830
esracosgun <esra.cosgun@gmx.de>
2022-06-24 15:27:04 +0200

    server

--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -17,7 +17,13 @@ import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
+import time 
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+import threading
 
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
 
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
@@ -27,33 +33,49 @@ class ServerAgent(InverseKinematicsAgent):
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.target_joints.get(joint_name)
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.target_joints[joint_name] = angle
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.posture
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyfames = keyframes
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.transfroms.get(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.set_transforms(effector_name, transform)
 
 if __name__ == '__main__':
     agent = ServerAgent()
+
+    server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler)
+    server.register_instance(agent)
+    server.register_introspection_functions()
+    server.register_multicall_functions()
+
+    thread = threading.Thread(target=server-serve_forever)
+    thread.start()
+    print('Serving XML-RPC on localhost port 8000')
+
     agent.run()
 
